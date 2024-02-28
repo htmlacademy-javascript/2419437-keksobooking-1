7d30978a78afdaf8a55c1224ee44b36cd7b94f14
Keks <keks@htmlacademy.ru>
2024-02-28 10:40:19 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/form.js
@@ -0,0 +1,265 @@
+import { similarAdvertisement } from './data.js';
+import { createCard } from './template-renderer.js';
+
+const mapFilters = document.querySelector('.map__filters');
+const mapFiltersItems = mapFilters.querySelectorAll(
+  'input, select, button, fieldset'
+);
+const adForm = document.querySelector('.ad-form');
+const adFormFieldset = adForm.querySelectorAll('fieldset');
+
+const disableMapFilters = () => {
+  if (mapFilters) {
+    mapFilters.classList.add('map__filters--disabled');
+
+    for (let i = 0; i < mapFiltersItems.length; i++) {
+      mapFiltersItems[i].disabled = true;
+    }
+  }
+};
+
+const activeMapFilters = () => {
+  if (mapFilters) {
+    mapFilters.classList.remove('map__filters--disabled');
+
+    for (let i = 0; i < mapFiltersItems.length; i++) {
+      mapFiltersItems[i].disabled = false;
+    }
+  }
+};
+
+const disableUserAdForm = () => {
+  if (adForm) {
+    adForm.classList.add('ad-form--disabled');
+
+    for (let i = 0; i < adFormFieldset.length; i++) {
+      adFormFieldset[i].disabled = true;
+    }
+  }
+};
+
+const activeUserAdForm = () => {
+  if (adForm) {
+    adForm.classList.remove('ad-form--disabled');
+
+    for (let i = 0; i < adFormFieldset.length; i++) {
+      adFormFieldset[i].disabled = false;
+    }
+  }
+};
+
+const pristine = new Pristine(adForm, {
+  classTo: 'form__item',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'form__item',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error',
+});
+
+function validateTitle(value) {
+  return value.length >= 30 && value.length <= 100;
+}
+
+function validateEmpty(value) {
+  return value.trim() !== '';
+}
+
+pristine.addValidator(
+  adForm.querySelector('#title'),
+  validateTitle,
+  'От 30 до 100 символов'
+);
+
+pristine.addValidator(adForm.querySelector('#address'), validateEmpty);
+
+function validateMaxValue(value) {
+  const intValue = parseInt(value, 10);
+  return !isNaN(intValue) && intValue <= 100000;
+}
+
+const amountPrice = adForm.querySelector('#price');
+
+const minAmountPrice = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+
+function validateMinAmount(value) {
+  const option = adForm.querySelector('#type option:checked');
+  return value.length && parseInt(value, 10) >= minAmountPrice[option.value];
+}
+
+function getMinAmountErrorMessage() {
+  const option = adForm.querySelector('#type option:checked');
+  return `Минимальная цена за ночь ${minAmountPrice[option.value]}`;
+}
+
+function onTypeChange() {
+  amountPrice.placeholder = minAmountPrice[this.value];
+  pristine.validate(amountPrice);
+}
+
+adForm
+  .querySelectorAll('#type')
+  .forEach((item) => item.addEventListener('change', onTypeChange));
+
+pristine.addValidator(amountPrice, validateMinAmount, getMinAmountErrorMessage);
+
+const roomsField = adForm.querySelector('[name="rooms"]');
+const capacityField = adForm.querySelector('[name="capacity"]');
+
+const roomsOption = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0'],
+};
+
+const checkTimeIn = adForm.querySelector('#timein');
+const checkTimeOut = adForm.querySelector('#timeout');
+function onTimeChange(event) {
+  const targetSelect = event.target;
+  const otherSelect = targetSelect === checkTimeIn ? checkTimeOut : checkTimeIn;
+
+  otherSelect.value = targetSelect.value;
+}
+
+checkTimeIn.addEventListener('change', onTimeChange);
+checkTimeOut.addEventListener('change', onTimeChange);
+
+function validateRooms() {
+  return roomsOption[roomsField.value].includes(capacityField.value);
+}
+
+pristine.addValidator(
+  capacityField,
+  validateRooms,
+  'Не подходит количество комнат'
+);
+
+pristine.addValidator(amountPrice, validateEmpty);
+
+pristine.addValidator(
+  amountPrice,
+  validateMaxValue,
+  'Максимальное значение — 100 000'
+);
+
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const isValid = pristine.validate();
+
+  if (isValid) {
+    adForm.submit();
+    // eslint-disable-next-line no-console
+    console.log('Форма валидна.');
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('Форма не валидна');
+  }
+});
+
+disableUserAdForm();
+disableMapFilters();
+
+//Map
+const map = L.map('map-canvas')
+  .on('load', () => {
+    activeUserAdForm();
+    activeMapFilters();
+  })
+  .setView(
+    {
+      lat: 35.6895,
+      lng: 139.6917,
+    },
+    9
+  );
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const mainMapIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const regularMapIcon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 52],
+});
+
+const mainCustomMarker = L.marker(
+  {
+    lat: 35.6895,
+    lng: 139.6917,
+  },
+  {
+    draggable: true,
+    icon: mainMapIcon,
+  }
+);
+
+similarAdvertisement.forEach((advertisement) => {
+  const {lat, lng} = advertisement.location;
+
+  const marker = L.marker(
+    {
+      lat,
+      lng,
+    },
+    {
+      draggable: false,
+      icon: regularMapIcon,
+    }
+  );
+
+  marker
+    .addTo(map)
+    .bindPopup(createCard(advertisement));
+});
+
+mainCustomMarker.addTo(map);
+
+mainCustomMarker.on('moveend', (evt) => {
+  const LatLng = evt.target.getLatLng();
+  adForm.querySelector('#address').value = `${LatLng.lat.toFixed(
+    5
+  )}, ${LatLng.lng.toFixed(5)}`;
+});
+
+
+// noUiSlider
+const sliderElement = document.querySelector('.ad-form__slider');
+const valueElement = document.querySelector('#price');
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100000,
+  },
+  start: 3000,
+  step: 100,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      return value.toFixed(0);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
+
+
+sliderElement.noUiSlider.on('update', () => {
+  valueElement.value = sliderElement.noUiSlider.get();
+});
---
+++ 9/js/template-renderer.js
@@ -0,0 +1,89 @@
+import { similarAdvertisement } from './data.js';
+
+export const typeOfHousing = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalow: 'Бунгало',
+  hotel: 'Отель',
+};
+
+export function createCard(advertisement) {
+  const cardTemplate = document.querySelector('#card').content;
+  const newItemTemplate = cardTemplate.querySelector('.popup');
+  const card = newItemTemplate.cloneNode(true);
+
+  const cardTitle = card.querySelector('.popup__title');
+  const cardAddress = card.querySelector('.popup__text--address');
+  const cardPrice = card.querySelector('.popup__text--price');
+
+  const cardType = card.querySelector('.popup__type');
+  const itemType = advertisement.offer.type;
+
+  const cardCapacity = card.querySelector('.popup__text--capacity');
+  const cardTime = card.querySelector('.popup__text--time');
+
+  const cardFeaturesContainer = card.querySelector('.popup__features');
+  const cardFeature = cardFeaturesContainer.querySelectorAll('.popup__feature');
+  const cardFeatures = advertisement.offer.features;
+
+  const cardDescription = card.querySelector('.popup__description');
+
+  const cardPhotosContainer = card.querySelector('.popup__photos');
+  const cardPhotoTemplate = cardPhotosContainer.querySelector('.popup__photo');
+  const photosArray = advertisement.offer.photos;
+
+  const cardAvatar = card.querySelector('.popup__avatar');
+
+  const comparisonType = Object.keys(typeOfHousing).some(
+    (key) => key === itemType
+  );
+
+  if (comparisonType) {
+    cardType.textContent = typeOfHousing[itemType];
+  }
+
+  cardFeature.forEach((cardFeatureItem) => {
+    const isNecessary = cardFeatures.some((feature) =>
+      cardFeatureItem.classList.contains(`popup__feature--${feature}`)
+    );
+
+    if (!isNecessary) {
+      cardFeatureItem.remove();
+    }
+  });
+
+  cardPhotosContainer.innerHTML = '';
+  photosArray.forEach((photoSrc) => {
+    const newPhoto = cardPhotoTemplate.cloneNode(true);
+    newPhoto.src = photoSrc;
+    cardPhotosContainer.appendChild(newPhoto);
+  });
+
+  cardTitle.textContent = advertisement.offer.title;
+  cardAddress.textContent = advertisement.offer.address;
+  cardPrice.textContent = `${advertisement.offer.price} ₽/ночь`;
+  cardCapacity.textContent = `${advertisement.offer.rooms} комнаты для ${advertisement.offer.guests} гостей`;
+  cardTime.textContent = `Заезд после ${advertisement.offer.checkin},выезд до ${advertisement.offer.checkout}`;
+  cardAvatar.src = advertisement.autor.avatar;
+
+  if (advertisement.offer.description) {
+    cardDescription.textContent = advertisement.offer.description;
+  } else {
+    cardDescription.style.display = 'none';
+  }
+
+  return card;
+}
+
+// Используем функцию createCard для каждого объявления
+let cardCounter = 0;
+const mapCanvas = document.querySelector('#map-canvas');
+
+for (let i = 0; i < similarAdvertisement.length; i++) {
+  const card = createCard(similarAdvertisement[i]);
+  if (!cardCounter) {
+    mapCanvas.appendChild(card);
+    cardCounter = 1;
+  }
+}
